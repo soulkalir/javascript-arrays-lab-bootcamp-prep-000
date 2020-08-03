35449c0e636cbb793762194657684ce9931ab895
Andres Rivera <soulkalir@gmail.com>
2020-08-03 00:15:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield" ] //define your array here
 
 // Add your functions and code here
+function appendKitten(kitten){
+  var newArray =[];
+  newArray= kittens.concat(kitten);
+return newArray;
+}
+
+function destructivelyAppendKitten(kitten){
+  kittens.push(`${kitten}`);
+}
+
+
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(`${kitten}`);
+}
+
+
+function destructivelyRemoveLastKitten(kitten){
+  kittens.pop(kitten);
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  kittens.shift(kitten);
+  }
+  
+  
+  function prependKitten(kitten){
+  var newArray= [kitten,...kittens];
+return newArray;
+  }
+  
+  
+  
+  function removeLastKitten(kitten){
+   return kittens.slice(0, -1)
+  }
+  
+  
+  function removeFirstKitten(kitten){
+    return kittens.slice(1)
+  }
